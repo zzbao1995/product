a4135442daf638f89a4d3349eb9483302d637fef
Jack Zhai <zhaizhenbao@inspur.com>
2018-08-29 13:37:26 +0800

    image资源路径修改

--- src/app/inventory-dept/inventory/inventory.component.ts
+++ src/app/inventory-dept/inventory/inventory.component.ts
@@ -16,19 +16,19 @@ export class InventoryComponent implements OnInit {
       new Product(
         'MYSHOES',
         'Black Running Shoes',
-        '../../assets/black-shoes.jpg',
+        '../../../assets/resources/images/products/black-shoes.jpg',
         ['Men', 'Shoes', 'Running Shoes'],
         109.99),
       new Product(
         'NEATOJACKET',
         'Blue Jacket',
-        '../../assets/blue-jacket.jpg',
+        '../../../assets/resources/images/products/blue-jacket.jpg',
         ['Women', 'Apparel', 'Jackets & Vests'],
         238.99),
       new Product(
         'NICEHAT',
         'A Nice Black Hat',
-        '../../assets/black-hat.jpg',
+        '../../../assets/resources/images/products/black-hat.jpg',
         ['Men', 'Accessories', 'Hats'],
         29.99)
     ];
